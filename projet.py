7fa1a13f7ae6db8cf32ef3b08f00da78a9796f54
douniamouchrif <102798610+douniamouchrif@users.noreply.github.com>
2022-04-20 20:03:50 +0200

    Update projet.py
    table (base de données)

--- projet.py
+++ projet.py
@@ -33,3 +33,49 @@ cursor.execute('''
     rate_Germ REAL
     );
 ''')
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS station (
+    id_s INTEGER PRIMARY KEY AUTOINCREMENT, 
+    nom TEXT NOT NULL,
+    range REAL,
+    altitude REAL
+    );
+''')
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS vallee (
+    id_v INTEGER PRIMARY KEY AUTOINCREMENT,
+    nom TEXT NOT NULL
+    );
+''')
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS arbre_station ( 
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    arbre_id INT NOT NULL,
+    station_id INT NOT NULL,
+    FOREIGN KEY (arbre_id) REFERENCES arbre (id_a),
+    FOREIGN KEY (station_id) REFERENCES station (id_s)
+    );
+''')
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS arbre_recolte (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    arbre_id INT NOT NULL, 
+    recolte_id INT NOT NULL, 
+    FOREIGN KEY (arbre_id) REFERENCES arbre (id_a), 
+    FOREIGN KEY (recolte_id) REFERENCES recolte (id_r)
+    );
+''')
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS station_vallee (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    station_id INT NOT NULL, 
+    vallee_id INT NOT NULL, 
+    FOREIGN KEY (station_id) REFERENCES station (id_s), 
+    FOREIGN KEY (vallee_id) REFERENCES vallee (id_v)
+    );
+''')
