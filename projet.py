bfbb5e0d79e9ad41a3454e115b8d5b5218465fa2
douniamouchrif <102798610+douniamouchrif@users.noreply.github.com>
2022-05-12 22:08:13 +0200

    Update projet.py

--- projet.py
+++ projet.py
@@ -85,3 +85,6 @@ with open('Repro_IS.csv', 'r') as csvfile:
             cursor.execute('''INSERT INTO arbre (id_a , code , VH , H , SH) VALUES ({},"{}","{}","{}","{}")'''.format(
                 variable2_id, row['code'], row['VH'], row['H'], row['SH']))
             variable2_id = variable2_id+1
+
+connexion.commit()
+connexion.close()
