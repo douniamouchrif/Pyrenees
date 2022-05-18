08df0d775fe7b51a2579d215724f4706797cd901
suhailaabarkan <102798630+suhailaabarkan@users.noreply.github.com>
2022-05-18 14:33:26 +0200

    Update GUI.py

--- view/GUI.py
+++ view/GUI.py
@@ -13,3 +13,11 @@ def build_graph_pie_chart(dataa, year):
     graphh = px.sunburst(dataa, path=['nom_v', 'nom_s', 'code'], values='Ntot', color='nom_s',
                          title='Evolution des récoltes – Year {}'.format(year))
     return graphh
+
+
+def build_scatter(df, year):
+    fig = px.scatter(df, x="SH", y="Ntot", animation_group="Ntot", animation_frame="Year",
+                     size="rate_Germ", color="nom_s", hover_name="nom_v", log_x=True, log_y=True,
+                     trendline="ols", trendline_scope="overall", trendline_color_override="black",
+                     title='Evolution des récoltes – Year {}'.format(year))
+    return fig
