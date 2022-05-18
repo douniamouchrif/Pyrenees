996fe72547b21c6e5caeb3a7d663dbe6001be04c
douniamouchrif <102798610+douniamouchrif@users.noreply.github.com>
2022-05-18 21:45:30 +0200

    Update GUI.py

--- view/GUI.py
+++ view/GUI.py
@@ -2,11 +2,18 @@ from dash import dcc
 import plotly.express as px
 
 
-def build_dropdown_menu(item_list):
+def build_dropdown_multi(item_list):
     options = [{"label": x, "value": x} for x in item_list]
     return dcc.Dropdown(id='dropdown',
                         options=options,
                         multi=True)
+
+
+def build_dropdown_select(item_list):
+    options = [{"label": x, "value": x} for x in item_list]
+    return dcc.Dropdown(id='dropdown',
+                        options=options,
+                        value=item_list[0])
 
 
 def build_graph_pie_chart(dataa, year):
@@ -20,4 +27,22 @@ def build_scatter(df, year):
                      size="rate_Germ", color="nom_s", hover_name="nom_v", log_x=True, log_y=True,
                      trendline="ols", trendline_scope="overall", trendline_color_override="black",
                      title='Evolution des récoltes – Year {}'.format(year))
+    return fig
+
+
+def create_time_series(dff, k):
+
+    fig = px.scatter(dff, x='Year', y=k)
+
+    fig.update_traces(mode='lines+markers')
+
+    fig.update_xaxes(showgrid=False)
+
+    fig.update_yaxes(type='linear')
+
+    fig.add_annotation(x=0, y=0.85, xanchor='left', yanchor='bottom',
+                       xref='paper', yref='paper', showarrow=False, align='left')
+
+    fig.update_layout(height=225, margin={'l': 20, 'b': 30, 'r': 10, 't': 10})
+
     return fig
